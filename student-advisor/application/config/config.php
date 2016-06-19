203c051941064015a6c4cf4f2296fe33e22aed6b
vesic <vvvesic@yahoo.com>
2016-06-20 01:28:05 +0200

    cloudApp

--- student-advisor/application/config/config.php
+++ student-advisor/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = "http://localhost/student-advisor";
+$config['base_url'] = "http://teststudent-advisor.azurewebsites.net/student-advisor";
 
 /*
 |--------------------------------------------------------------------------
